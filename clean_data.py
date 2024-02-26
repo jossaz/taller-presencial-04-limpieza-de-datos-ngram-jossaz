e14b5fd63a8bee5423f6c8ab6729f2098933eef2
jossaz <51805547+jossaz@users.noreply.github.com>
2024-02-26 15:29:59 -0500

    Add files via upload

--- clean_data.py
+++ clean_data.py
@@ -6,6 +6,9 @@ import pandas as pd
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
 
+    #
+    # Esta parte es igual al taller anterior
+    #
     df = pd.read_csv(input_file)
     return df
 
@@ -14,38 +17,30 @@ def create_key(df, n):
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
 
     df = df.copy()
-
-    # Copie la columna 'text' a la columna 'key'
     df["key"] = df["text"]
-
-    # Remueva los espacios en blanco al principio y al final de la cadena
     df["key"] = df["key"].str.strip()
-
-    # Convierta el texto a minúsculas
     df["key"] = df["key"].str.lower()
-
-    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
     df["key"] = df["key"].str.replace("-", "")
-
-    # Remueva puntuación y caracteres de control
     df["key"] = df["key"].str.translate(
         str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
     )
-
-    # Convierta el texto a una lista de tokens
     df["key"] = df["key"].str.split()
 
-    # Una el texto sin espacios en blanco
+    # ------------------------------------------------------
+    # Esta es la parte especifica del algoritmo de n-gram:
+    #
+    # - Una el texto sin espacios en blanco
     df["key"] = df["key"].str.join("")
-   
-    # Convierta el texto a una lista de n-gramas
-    df["key"] = df["key"].map(lambda x: [x[t:t+n-1] for t in range(len(x))])
-
-    # Ordene la lista de n-gramas y remueve duplicados
+    #
+    # - Convierta el texto a una lista de n-gramas
+    df["key"] = df["key"].map(lambda x: [x[t : t + n - 1] for t in range(len(x))])
+    #
+    # - Ordene la lista de n-gramas y remueve duplicados
     df["key"] = df["key"].apply(lambda x: sorted(set(x)))
-
-    # Convierta la lista de ngramas a una cadena
+    #
+    # - Convierta la lista de ngramas a una cadena
     df["key"] = df["key"].str.join("")
+    ## ------------------------------------------------------
 
     return df
 
@@ -53,18 +48,13 @@ def create_key(df, n):
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
+    #
+    # Este código es identico al anteior
+    #
     df = df.copy()
-
-    # Ordene el dataframe por 'key' y 'text'
     df = df.sort_values(by=["key", "text"], ascending=[True, True])
-
-    # Seleccione la primera fila de cada grupo de 'key'
     keys = df.drop_duplicates(subset="key", keep="first")
-
-    # Cree un diccionario con 'key' como clave y 'text' como valor
     key_dict = dict(zip(keys["key"], keys["text"]))
-
-    # Cree la columna 'cleaned' usando el diccionario
     df["cleaned"] = df["key"].map(key_dict)
 
     return df
@@ -72,7 +62,9 @@ def generate_cleaned_column(df):
 
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
-
+    #
+    # Este código es identico al anteior
+    #
     df = df.copy()
     df = df[["cleaned"]]
     df = df.rename(columns={"cleaned": "text"})
@@ -81,7 +73,9 @@ def save_data(df, output_file):
 
 def main(input_file, output_file, n=2):
     """Ejecuta la limpieza de datos"""
-
+    #
+    # Este código es identico al anteior
+    #
     df = load_data(input_file)
     df = create_key(df, n)
     df = generate_cleaned_column(df)
--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 
 import os
 
-import pandas as pd
 import clean_data
+import pandas as pd
 
 clean_data.main("input.txt", "output.txt")
 
@@ -16,7 +16,6 @@ assert test.loc[0, "key"] == "acdeghinoqruy"
 assert test.loc[1, "key"] == "acdegilmnoty"
 assert test.loc[3, "key"] == "acdehioqrsu"
 assert test.loc[6, "key"] == "acdehoqruy"
-assert test.loc[8, "key"] == "acdeilmnosty"
 assert test.loc[12, "key"] == "acdeilmnoty"
 assert test.loc[16, "key"] == "acdgilnoprstu"
 
@@ -31,9 +30,9 @@ dataframe = pd.read_csv("output.txt")
 count = dataframe.groupby("text").size()
 
 
-assert count.loc["AD-HOC QUERIES"] == 6
-assert count.loc["AGRICULTURAL PRODUCTION"] == 5
-assert count.loc["AIRLINE COMPANIES"] == 4
+assert count.loc["AD-HOC QUERIES"] == 3
+assert count.loc["AGRICULTURAL PRODUCTION"] == 1
+assert count.loc["AIRLINE COMPANIES"] == 1
 assert count.loc["AIRLINES"] == 1
 assert count.loc["ANALYTIC APPLICATIONS"] == 9
-assert count.loc["ANALYTIC MODEL"] == 10
+assert count.loc["ANALYTIC MODEL"] == 4
