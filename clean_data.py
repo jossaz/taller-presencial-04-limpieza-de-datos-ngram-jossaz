e57511a7fbdc873e2bb6b1a3ba34314c976774a5
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-03-20 07:57:35 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -49,7 +49,8 @@ def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     #
-    # Este código es identico al anterior
+    # Este código es identico al anteior
+    #
     df = df.copy()
     df = df.sort_values(by=["key", "text"], ascending=[True, True])
     keys = df.drop_duplicates(subset="key", keep="first")
